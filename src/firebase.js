0f5df49c67b35956a617bd214dee2e07a4b4446b
tund <tund@gemvietnam.com>
2024-04-25 09:54:37 +0700

    update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -8,18 +8,28 @@ import { getMessaging, onMessage } from "firebase/messaging";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArQo0TUhlE1QDfaG4PRC-xXY9tYBA_-eo",
-  authDomain: "login-fire-base-app.firebaseapp.com",
-  projectId: "login-fire-base-app",
-  storageBucket: "login-fire-base-app.appspot.com",
-  messagingSenderId: "67526657392",
-  appId: "1:67526657392:web:97ea621e3e62d89a4cfbb5",
-  measurementId: "G-DWESQGQ07Q",
+  // apiKey: "AIzaSyArQo0TUhlE1QDfaG4PRC-xXY9tYBA_-eo",
+  // authDomain: "login-fire-base-app.firebaseapp.com",
+  // projectId: "login-fire-base-app",
+  // storageBucket: "login-fire-base-app.appspot.com",
+  // messagingSenderId: "67526657392",
+  // appId: "1:67526657392:web:97ea621e3e62d89a4cfbb5",
+  // measurementId: "G-DWESQGQ07Q",
+
+  apiKey: "AIzaSyDsfU6kWMS9xjiLPWlDq2_CCIlJd2p-QIM",
+  authDomain: "social-network-login-app.firebaseapp.com",
+  projectId: "social-network-login-app",
+  storageBucket: "social-network-login-app.appspot.com",
+  messagingSenderId: "619944841448",
+  appId: "1:619944841448:web:09bb7a4926d29e4206f95d",
+  measurementId: "G-QJF0MYQPM3",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const messaging = getMessaging(app);
+
+console.log(messaging, "messaging of firebase component");
 
 export const onMessageListener = () =>
   new Promise((resolve) => {
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,13 +5,21 @@ import app, { messaging, onMessageListener } from "../../firebase";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArQo0TUhlE1QDfaG4PRC-xXY9tYBA_-eo",
-  authDomain: "login-fire-base-app.firebaseapp.com",
-  projectId: "login-fire-base-app",
-  storageBucket: "login-fire-base-app.appspot.com",
-  messagingSenderId: "67526657392",
-  appId: "1:67526657392:web:97ea621e3e62d89a4cfbb5",
-  measurementId: "G-DWESQGQ07Q",
+  // apiKey: "AIzaSyArQo0TUhlE1QDfaG4PRC-xXY9tYBA_-eo",
+  // authDomain: "login-fire-base-app.firebaseapp.com",
+  // projectId: "login-fire-base-app",
+  // storageBucket: "login-fire-base-app.appspot.com",
+  // messagingSenderId: "67526657392",
+  // appId: "1:67526657392:web:97ea621e3e62d89a4cfbb5",
+  // measurementId: "G-DWESQGQ07Q",
+
+  apiKey: "AIzaSyDsfU6kWMS9xjiLPWlDq2_CCIlJd2p-QIM",
+  authDomain: "social-network-login-app.firebaseapp.com",
+  projectId: "social-network-login-app",
+  storageBucket: "social-network-login-app.appspot.com",
+  messagingSenderId: "619944841448",
+  appId: "1:619944841448:web:09bb7a4926d29e4206f95d",
+  measurementId: "G-QJF0MYQPM3",
 };
 
 export default function Home() {
@@ -28,6 +36,8 @@ export default function Home() {
     const firebase = initializeApp(firebaseConfig);
     const messaging = getMessaging(firebase);
 
+    console.log(messaging, "messaging of home page");
+
     onMessage(messaging, (payload) => {
       console.log("payload", payload);
     });
@@ -37,6 +47,9 @@ export default function Home() {
 
   const requestPermission = async () => {
     const permission = await Notification.requestPermission();
+
+    console.log(permission, "permission 001");
+
     if (permission === "granted") {
       const currentToken = await getToken(messaging, {
         vapidKey:
